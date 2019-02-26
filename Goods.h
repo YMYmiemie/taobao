91f17fbbdca20c8f775c44cd3b20e84881fe13df
YMYmiemie <43571569+YMYmiemie@users.noreply.github.com>
2019-02-26 15:51:19 +0800

    Add files via upload

---
+++ Goods.h
@@ -0,0 +1,12 @@
+#pragma once
+#include<iostream>
+using namespace std;
+
+struct goods
+{
+	string ID = "null";
+	string name = "null";
+	string brand = "null";
+	float price = 0.0;
+	int num = 0;
+};
